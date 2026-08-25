4197d8ee911b37c8f09b05f32c5b9118de81b415
paarth2023 <paarth.mahadik@gmail.com>
2026-08-25 11:56:58 +0530

    modularized the chip8 fetch - decode structure, added TODOs folder to keep track of objectives, started with documentation

---
+++ TODOs/TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+## 25-8-2026
+
+- [X] Modularize the fetch-decode-execute cycle chip8 structure.
+- [ ] Start documentation.
--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,6 @@
 #define u8 uint8_t
 #define u16 uint16_t
 
-typedef struct chip8
-{
-    u8 memory[4096];
-    u16 stack[16]; // stack;
-    u8 registers[16];
-
-    // special registers
-    u16 iRegister;
-    u16 pc;      // program counter
-    u8 delay;    // delay timer
-    u8 sound;    // sound timer
-    u8 sPointer; // stack pointer
-
-    int romSize;
-
-    // display
-    u8 display[64 * 32];
-} chip8;
-
 u8 font[] = {
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
     0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -46,33 +27,144 @@ u8 font[] = {
     0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 
 };
-
-void loadRom(u8 memory[], int &setSize)
+typedef struct chip8
 {
-    std::ifstream file("IBM Logo.ch8", std::ios::binary);
-    file.seekg(0, file.end);
-    int romSize = file.tellg();
-    file.seekg(0, file.beg);
+    u8 memory[4096];
+    u16 stack[16]; // stack;
+    u8 registers[16];
 
-    setSize = romSize;
+    // special registers
+    u16 iRegister;
+    u16 pc;      // program counter
+    u8 delay;    // delay timer
+    u8 sound;    // sound timer
+    u8 sPointer; // stack pointer
 
-    char *buffer = new char[romSize];
+    int romSize;
 
-    file.read(buffer, romSize);
+    // display
+    u8 display[64 * 32];
 
-    for (int i = 0; i < romSize; i++)
+    // opcode (added during modularization)
+
+    u16 opcode;
+
+    bool jumpFlag;
+
+    void init()
     {
-        memory[0x200 + i] = buffer[i];
+        pc = 0x200;
     }
-}
 
-void clearDisplay(u8 *display)
-{
-    for (int i = 0; i < 64 * 32; i++)
+    void setFont()
     {
-        display[i] = 0;
+
+        int i = 0;
+        for (u8 a = 0x050; a <= 0x09F; a++)
+        {
+            memory[a] = font[i];
+            i++;
+        }
     }
-}
+
+    void clearScreen()
+    {
+        for (int i = 0; i < 64 * 32; i++)
+            display[i] = 0;
+    }
+
+    void loadRom()
+    {
+
+        std::ifstream file("IBM Logo.ch8", std::ios::binary);
+        file.seekg(0, file.end);
+        int romSize = file.tellg();
+        file.seekg(0, file.beg);
+
+        char *buffer = new char[romSize];
+
+        file.read(buffer, romSize);
+
+        for (int i = 0; i < romSize; i++)
+        {
+            memory[0x200 + i] = buffer[i];
+        }
+    }
+
+    void fetch()
+    {
+        opcode = (memory[pc] << 8) | memory[pc + 1];
+    }
+
+    void decode()
+    {
+        jumpFlag = false;
+        switch (opcode & 0xF000)
+        {
+        case 0x0000:
+            if (opcode == 0x00E0)
+                clearScreen();
+            if (opcode == 0x0000)
+                return;
+            break;
+        case 0x1000:
+            pc = (opcode & 0x0FFF);
+            jumpFlag = true;
+            break;
+        case 0x6000:
+            registers[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
+            break;
+        case 0x7000:
+            registers[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
+            break;
+        case 0xA000:
+            iRegister = opcode & 0x0FFF;
+            break;
+        case 0xD000:
+        {
+            // DXYN
+            auto X = (opcode & 0x0F00) >> 8;
+            auto Y = (opcode & 0x00F0) >> 4;
+            X = registers[X] & 63; // fancy way of doing modulo
+            Y = registers[Y] & 31;
+
+            registers[0xF] = 0;
+
+            auto N = opcode & 0x000F;
+
+            u8 sprite;
+            for (int i = 0; i < N; i++)
+            {
+                sprite = memory[iRegister + i];
+                for (int j = 7; j >= 0; j--)
+                {
+                    int bit = (sprite >> j) & 1;
+                    if (bit == 1)
+                    {
+
+                        int index = Y * 64 + X;
+                        if (display[index] == 255)
+                            registers[0xF] = 1;
+                        display[index] ^= 255;
+                    }
+                    X = (X + 1) % 64;
+                }
+
+                X = (opcode & 0x0F00) >> 8;
+                X = registers[X] & 63;
+
+                Y = (Y + 1) % 32;
+            }
+            break;
+        }
+        default:
+            break;
+        }
+
+        if (!jumpFlag)
+            pc += 2;
+    }
+} chip8;
 
 int main()
 {
@@ -82,103 +174,17 @@ int main()
 
     chip8 emulator{};
 
-    loadRom(emulator.memory, emulator.romSize);
+    emulator.loadRom();
 
-    int i = 0;
-    for (u8 a = 0x050; a <= 0x09F; a++)
-    {
-        emulator.memory[a] = font[i];
-        i++;
-    }
-
-    emulator.pc = 0x200;
+    emulator.init();
 
     while (!renderer.shouldClose())
     {
         // fetch;
-        u16 opcode = (emulator.memory[emulator.pc] << 8) | emulator.memory[emulator.pc + 1];
+        emulator.fetch();
 
         // decode
-        bool jump = false;
-        switch (opcode & 0xF000)
-        {
-        case 0x0000:
-            printf("+++++++++\n");
-            if (opcode == 0x00E0)
-                clearDisplay(emulator.display);
-            if (opcode == 0x0000)
-                return 1;
-            break;
-        case 0x1000:
-            printf("---------\n");
-            emulator.pc = (opcode & 0x0FFF);
-            jump = true;
-            break;
-        case 0x6000:
-            printf("888888888\n");
-            emulator.registers[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
-            break;
-        case 0x7000:
-            printf("77777777\n");
-            emulator.registers[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
-            break;
-        case 0xA000:
-            printf("AAAAAAAA\n");
-            emulator.iRegister = opcode & 0x0FFF;
-            break;
-        case 0xD000:
-        {
-            // DXYN
-            printf("DDDDDDDDDD\n");
-            auto X = (opcode & 0x0F00) >> 8;
-            auto Y = (opcode & 0x00F0) >> 4;
-            X = emulator.registers[X] & 63; // fancy way of doing modulo
-            Y = emulator.registers[Y] & 31;
-
-            emulator.registers[0xF] = 0;
-
-            auto N = opcode & 0x000F;
-
-            u8 sprite;
-            for (int i = 0; i < N; i++)
-            {
-                sprite = emulator.memory[emulator.iRegister + i];
-                for (int j = 7; j >= 0; j--)
-                {
-                    int bit = (sprite >> j) & 1;
-                    if (bit == 1)
-                    {
-
-                        int index = Y * 64 + X;
-                        if (emulator.display[index] == 255)
-                            emulator.registers[0xF] = 1;
-                        emulator.display[index] ^= 255;
-                    }
-                    X = (X + 1) % 64;
-                }
-
-                X = (opcode & 0x0F00) >> 8;
-                X = emulator.registers[X] & 63;
-
-                Y = (Y + 1) % 32;
-            }
-            for (int y = 0; y < 32; y++)
-            {
-                for (int x = 0; x < 64; x++)
-                {
-                    printf("%c", emulator.display[y * 64 + x] ? '#' : '.');
-                }
-
-                printf("\n");
-            }
-            break;
-        }
-        default:
-            break;
-        }
-
-        if (!jump)
-            emulator.pc += 2;
+        emulator.decode();
         renderer.render(emulator.display);
         renderer.pollEvents();
     }
